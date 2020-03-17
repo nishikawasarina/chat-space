f1aa9f8df147f598621fe81982aee2c9098558a2
nishikawasarina <sarisari.love@icloud.com>
2020-03-18 02:25:31 +0900

    自動更新機能

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,9 +1,9 @@
 $(function(){
 
-  function buildHTML(message){
-    if (message.image) {
+  var buildHTML = function(message) {
+    if (message.content && message.image) {
       var html =
-        `<div class="message">
+        `<div class="message" data-message-id=${message.id}>
           <div class="message__user">
             <div class="message__user--name">
               ${message.user_name}
@@ -19,10 +19,9 @@ $(function(){
             <img class="lower-message__image" src=${message.image} >
           </div>
         </div>`
-      return html;
-    } else {
+    } else if (message.content) {
       var html =
-        `<div class="message">
+        `<div class="message" data-message-id=${message.id}>
           <div class="message__user">
             <div class="message__user--name">
               ${message.user_name}
@@ -37,9 +36,24 @@ $(function(){
             </p>
           </div>
         </div>`
-      return html;
+    }  else if (message.image) {
+      var html =
+        `<div class="message" data-message-id=${message.id}>
+          <div class="message__user">
+            <div class="message__user--name">
+              ${message.user_name}
+            </div>
+            <div class="message__user--time">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message__text">
+            <img class="lower-message__image" src=${message.image} >
+          </div>
+        </div>`
     };
-  }
+    return html;
+  };
 
   $('#new_message').on('submit', function(e){
     e.preventDefault()
@@ -64,4 +78,38 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     });
   });
+
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        //追加するHTMLの入れ物を作る
+        var insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.messages').append(insertHTML);
+        $('.main-chat__message-list').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  //$(function(){});の閉じタグの直上(処理の最後)に以下のように追記
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
